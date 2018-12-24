531848bedacba1059a07771ca57c16b1aed5ce90
Michal Muszynski <m.muszy@gmail.com>
2018-12-24 12:44:32 +0100

    Add color object

--- src/Components/Game/Game.js
+++ src/Components/Game/Game.js
@@ -5,12 +5,25 @@ import _ from 'lodash'
 
 const randomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+const colors = {
+  new: '#008B8B',
+  playing: '#6495ED',
+  won: '#3CB371',
+  lost: '#F08080'
+}
+
 class Game extends Component {
 
   state = {
     gameStatus: 'new',
     remainingSeconds: this.props.initialSeconds,
     selectedIds: []
+  }
+
+  notClickedColor = {
+    playing: '#ccc',
+    won: 'green',
+    lost: 'red'
   }
 
   challengeNumbers = Array.from({ length: this.props.challengeSize })
