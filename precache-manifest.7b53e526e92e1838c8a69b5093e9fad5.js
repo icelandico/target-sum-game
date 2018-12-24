9e0126a9f64a967f71c973c096ae6852b8655734
Michal Muszynski <m.muszy@gmail.com>
2018-12-24 14:15:00 +0100

    Updates

---
+++ precache-manifest.7b53e526e92e1838c8a69b5093e9fad5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "284969264de3cab3ca9a",
+    "url": "/target-sum-game/static/js/runtime~main.28496926.js"
+  },
+  {
+    "revision": "12baa08afe2e60e1420f",
+    "url": "/target-sum-game/static/js/main.12baa08a.chunk.js"
+  },
+  {
+    "revision": "29035163a0a7a20d685e",
+    "url": "/target-sum-game/static/js/1.29035163.chunk.js"
+  },
+  {
+    "revision": "12baa08afe2e60e1420f",
+    "url": "/target-sum-game/static/css/main.651d260c.chunk.css"
+  },
+  {
+    "revision": "3ceeaf0b8280b38507acb9f13a856140",
+    "url": "/target-sum-game/index.html"
+  }
+];
